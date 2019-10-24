9c7b7488579c3c7514ab8c8624f16303f649a687
termuxuser01 <53441033+termuxuser01@users.noreply.github.com>
2019-10-24 10:35:09 -0500

    Update sql_alpha.py
    Added forgotten commits.

--- Learn-python-programming-masterclass/sql_alpha.py
+++ Learn-python-programming-masterclass/sql_alpha.py
@@ -15,11 +15,15 @@ UPDATE_CURSOR = db.cursor()
 for t in zip(range(1,27), lw):
   UPDATE_CURSOR.execute(update_db, t)
 
+db.commit()
+ 
 update_db = "INSERT INTO upper_alpha VALUES(?, ?);"
 
 for t in zip(range(1,27), up):
   UPDATE_CURSOR.execute(update_db, t)
 
+db.commit()  
+  
 for i, l, u in db.execute("SELECT alpha.id, alpha.letter, upper_alpha.letteru FROM alpha JOIN upper_alpha ON alpha.id = upper_alpha.id"):
   print(f'id: {i}\nletter: {l}\nuppercase: {u}')
   print("-" * 40)
