8be878a3e62c1b67dab50c037680a03cb1e2062c
termuxuser01 <53441033+termuxuser01@users.noreply.github.com>
2019-10-24 11:11:58 -0500

    Update rollback1.py
    updating progress.

--- Learn-python-programming-masterclass/rollback1.py
+++ Learn-python-programming-masterclass/rollback1.py
@@ -1,13 +1,33 @@
+import sqlite3
+
+db = sqlite3.connect("account.sqlite")
+
+db.execute("CREATE TABLE IF NOT EXISTS accounts (name TEXT PRIMARY KEY NOT NULL, balance INTEGER NOT NULL)")
+#deposits will be positive, widrawls negative.
+db.execute("CREATE TABLE IF NOT EXISTS transactions (time TIMESTAMP NOT NULL, account TEXT NOT NULL, amount INTEGER NOT NULL, PRIMARY KEY ( time, amount))")
+
+
 class Account(object):
 
-  def __init__(self, name: str, openning_balance: float = 0.0):
-    self.name = name
-    self._balance = openning_balance
-    print("account created succesfully for {}!".format(self.name))
+  def __init__(self, name: str, openning_balance: float = 0.0) -> None:
+    cursor = db.execute("SELECT name, balance FROM accounts WHERE (name = ?)", (name,))
+    row = cursor.fetchone()
+
+    if row:
+      self.name, self._balance = row
+      print("retrieved record for {}".format(self.name))
+    else:
+      self.name = name
+      self._balance = openning_balance
+      cursor.execute("INSERT INTO accounts VALUES(?, ?)", (name, openning_balance))
+      cursor.connection.commit()
+      print("account created succesfully for {}!".format(self.name))
+    self.show_blanace()
 
   def deposit(self, amount: float) -> float:
     if amount > 0.0:
-      self._balance += amount
+      new_balance = self._balance + amount
+      deposit_time = 
       print(f"deposited: ${amount}")
     else:
       print("cannot deposit negative quantity")
@@ -30,3 +50,5 @@ if __name__ == "__main__":
   Vianney.deposit(1337.50)
   Vianney.withdrawal(500)
   Vianney.show_blanace()
+
+  Veronica = Account("VIanney")
